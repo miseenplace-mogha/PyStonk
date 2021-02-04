7a45eb5a42adafb29a27e4e7d463a7dc337be2bf
miseenplace-mogha <78489775+miseenplace-mogha@users.noreply.github.com>
2021-02-03 23:16:22 -0500

    Update PricePlot.py
    added edits from home.
    **trying to figure out how to pull GitHub repository into PyCharm for editing.

--- PricePlot.py
+++ PricePlot.py
@@ -4,63 +4,92 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-url = r"https://finance.yahoo.com/quote/"
-stonklist = []
+power_on = str(input("ready to go?\n(y/n): ")).upper()
+if power_on == 'Y':
+    print("vroom vroom")
+    run = True
+else:
+    run = False
 
 
-def draw_pyplot(symbol):
-    plt.title("{} price graph".format(symbol))
-    plt.xlabel("Time")
-    plt.ylabel("Price ($)")
-## todo: configure axes min & max
-#    plt.axis(min(x_axis), max(x_axis), min(plot_price), max(plot_price))
-    plt.autoscale()
-    plt.plot(x_axis, plot_price, marker='o')
-    plt.show()
 
+## todo: write list comprehension for plt.plot function to adjust for input lists
+# def line_compiler():
+    # price_list = [
+    # time_list =
+    # lines = []
+    # for i in range(len(price_list)):
+    #    lines.append(price_list[i-1])
+    #    lines.append(time_list[i-1])
+    #
+
+## todo: flip while & for loops to get prices of different stocks at the same time interval
+# def stock_price_parallel():
+#    while time_count < maxtime:
+#        for i in stonklist:
 
 ## todo: create function that plots multiple stock prices in a single plot
-## todo: write list comprehension for plt.plot function to adjust for input lists
-## todo: flip while & for loops to get prices of different stocks at the same time interval
 # def multiline_chart(symbols):
+#    line_compiler()
 #    plt.title("Price graph".format(symbol))
 #    plt.xlabel("Time")
 #    plt.ylabel("Price ($)")
-#    plt.axis(min(x_axis), max(x_axis), min(plot_price), max(plot_price))
 #    plt.autoscale()
-#    plt.plot()
+#    plt.plot(lines)
 #    plt.show()
 
-stonk = str(input("Enter Stock Abbreviation: ")).upper()
-stonklist.append(stonk)
-while True:
-    looptest = str(input("Enter another stock, or enter 'n' to calculate: ")).upper()
-    if looptest != 'N':
-        stonklist.append(looptest)
-        continue
-    else:
-        break
-#print(stonklist)
-maxtime = int(input("How many seconds would you like to track the price? "))
-for i in stonklist:
-    plot_price = []
-    time_count = 0
-    x_axis = []
-    print("\n")
-    try:
-        while time_count < maxtime:
-            #start = time.time()
-            urla = url + i
-            YFpage = requests.get(urla)
-            soup = BeautifulSoup(YFpage.content, 'html.parser')
-            price = soup.find("span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).get_text(strip=True)
-            # print("The current price of ${} is {}".format(i, price))
-            plot_price.append(price)
-            print("found price of {} for time interval {}".format(i, time_count+1))
-            x_axis.append(time.strftime('%H:%M:%S', time.localtime()))
-            # print("total time taken this loop: ", time.time() - start)
-            time_count += 1
-            time.sleep(1)
-        draw_pyplot(i)
-    except:
-        print("I can't find that stock...\nHow awkward...\nMoving on...")
+
+def draw_pyplot(symbol, x_axis, y_axis):
+    ## todo: configure axes to be legible
+    ## todo: find how to auto-close plots
+    plt.title("{} price graph".format(symbol))
+    plt.xlabel("Time")
+    plt.ylabel("Price ($)")
+    plt.autoscale()
+    plt.plot(x_axis, y_axis)
+    plt.show()
+#    time.sleep(3)
+#    plt.close()
+
+
+def stock_price_linear():
+    base_url = r"https://finance.yahoo.com/quote/"
+    stonklist = []
+    stonk = str(input("Enter Stock Abbreviation: ")).upper()
+    stonklist.append(stonk)
+    while True:
+        looptest = str(input("Enter another stock, or enter 'n' to calculate: ")).upper()
+        if looptest != 'N':
+            stonklist.append(looptest)
+            continue
+        else:
+            break
+    print("I will now begin searching for the prices of {}".format(stonklist))
+    maxtime = int(input("How many seconds would you like to track the price? "))
+    for i in stonklist:
+        time_count = 0
+        y_axis = []
+        x_axis = []
+        print("\n")
+        try:
+            while time_count < maxtime:
+                #start = time.time()
+                url = base_url + i
+                YFpage = requests.get(url)
+                soup = BeautifulSoup(YFpage.content, 'html.parser')
+                price = soup.find("span", {"class": "Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"}).get_text(strip=True)
+                # print("The current price of ${} is {}".format(i, price))
+                y_axis.append(price)
+                print("found price of {} for time interval {}".format(i, time_count+1))
+                x_axis.append(time.strftime('%H:%M:%S', time.localtime()))
+                # print("total time taken this loop: ", time.time() - start)
+                time_count += 1
+                time.sleep(1)
+            draw_pyplot(i, x_axis, y_axis)
+        except:
+            print("I can't find Stock ${}...\nHow awkward...\nMoving on...".format(i))
+
+
+while run == True:
+#    print("testing...")
+    stock_price_linear()
